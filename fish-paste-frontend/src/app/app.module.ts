6dcc117bddf1160fe40d003493caa947c2b59d10
Luke Westfall <lwestfall1@gmail.com>
2020-11-21 12:00:25 -0500

    Add animation and reduce card width

--- fish-paste-frontend/src/app/app.module.ts
+++ fish-paste-frontend/src/app/app.module.ts
@@ -4,15 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
       HomeComponent
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
